60c242181d814ef21509e64630f297c8fa00eeda
robin <petar.bertok@gmail.com>
2020-02-24 22:23:59 +0100

    conn.php file

--- conn.php
+++ conn.php
@@ -1,7 +1,7 @@
 <?php
 $servername = "localhost";
-$username = "petar";
-$password = "PASSWORD";
+$username = "username";
+$password = "password";
 $database = "dropdown";
 
 $conn = mysqli_connect($servername, $username, $password, $database);
@@ -9,5 +9,5 @@ $conn = mysqli_connect($servername, $username, $password, $database);
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 }
-//echo "Connected successfully" . "<br />";
+//echo "Connected successfully" . "\n";
 ?>
